c59061aa57c446577574558a4b3aa9b490ad6699
Deanne <madulid.deanne@gmail.com>
2021-09-23 16:25:49 -0400

    Fixed positioning of Sign In button

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/NavBar'
 import Nav from 'react-bootstrap/Nav'
 import {BrowserRouter as Router, NavLink, Link} from 'react-router-dom'
@@ -6,20 +7,20 @@ import './NavigationBar.css'
 
 const NavigationBar = () => {
   return (
-    <>
     <Navbar bg="dark" variant="dark">
       <Router>
         <Navbar.Brand>
           <Link to="/"><div className="Home">HabitTracker</div></Link>
         </Navbar.Brand>
-        <Nav>
+        <Container>
+        <Nav className="justify-content-end">
           <Nav.Link>
             <NavLink to='/signin'>Sign In</NavLink>
           </Nav.Link>
         </Nav>
+        </Container>
       </Router>
     </Navbar>
-    </>
   )
 }
 
