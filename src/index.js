8426a4504f52e7815af8c68fd2ccfd12ea3b39a2
Deanne <madulid.deanne@gmail.com>
2021-10-07 22:53:56 -0400

    Added redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
+import habitsReducer from './components/Dashboard/habitsReducer'
+
+const reducer = combineReducers({
+  habits: habitsReducer
+})
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
